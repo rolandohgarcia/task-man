e3e04a66a3aabcfd047ed85570f400ee5759953f
Task-Man System <system@taskman.local>
2026-06-23 14:30:23 -0600

    chore: add massive debug block to catch FCM raw HTTP response

--- test_fcm.js
+++ test_fcm.js
@@ -3,6 +3,8 @@ fetch("https://fcmregistrations.googleapis.com/v1/projects/task-man-3ca46/regist
   headers: {
     'x-goog-api-key': "AIzaSyB277QJjuZjdlxC-lQvajd3ag3RYQNhKls",
     'x-goog-firebase-installations-auth': 'invalid_token_just_for_testing',
+    'Origin': 'https://task-man--task-man-3ca46.us-central1.hosted.app',
+    'Referer': 'https://task-man--task-man-3ca46.us-central1.hosted.app/',
     'Content-Type': 'application/json'
   },
   body: JSON.stringify({
