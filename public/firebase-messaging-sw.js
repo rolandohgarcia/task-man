550d52424f4b9236ca49063e3ac9c7c2340a5580
Task-Man System <system@taskman.local>
2026-06-23 17:04:17 -0600

    feat: add PWA manifest, favicon and app icons

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -27,7 +27,7 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/favicon.svg'
+    icon: '/tasks.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
